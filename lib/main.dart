418ac29db4aba89ab887f0f64202a146d45e41ae
victortelles <victor.telles@iteso.mx>
2025-02-11 22:25:37 -0600

    Estructura simple del proyecto + Funcionalidad para generar las contraseñas

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+//Proyecto: Generacion de contraseñas-Diferentes tamaños de caracteres.
+//Libreria de random
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,15 +12,70 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 117, 135, 237),
         ),
       ),
+      //Contenido
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //Lista de longitud de caracteres
+  final List<int> passwordLengths = [8, 12, 16, 20, 24, 28, 32, 36, 40];
+
+  //Mapa para almacenar las contraseñas generadas
+  final Map<int, String> passwords = {};
+
+  @override
+  void initState() {
+    super.initState();
+
+    //Inicializar valores por default
+    for (var lengh in passwordLengths) {
+      passwords[lengh] = 'Contraseñas sin guardar';
+    }
+  }
+
+  //Esqueleto
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Generador de contraseñas'),
+      ),
+      //Contenido
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              //Widget ListView
+              child: ListView(
+                //Implementar la logica
+
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+    //Funcion para generar la contraseña
+    String generarPassword(int charsNum) {
+    return String.fromCharCodes(
+      List.generate(charsNum, (index) => Random().nextInt(48) + 60),
     );
   }
 }
